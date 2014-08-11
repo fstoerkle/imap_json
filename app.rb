75a787bd6529b765772e6196ee95367655e13cc8
Florian Störkle <florian@stoerkle.net>
2014-08-11 17:45:57 +0200

    Save

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 #!/usr/bin/env ruby
 
+require_relative 'lib/email.rb'
 require_relative 'lib/files.rb'
 require_relative 'lib/imap.rb'
 
 imap = Imap.new
-imap.list(Configuration['mailbox']).each { |mailbox| puts mailbox.name }
+imap.list(Configuration['mailbox']).each do |mailbox|
+  imap.messages_for(mailbox.name) do |message|
+    puts message
+    exit
+  end
+  exit
+end
+
+
 
 # Files.create_directories
--- lib/imap.rb
+++ lib/imap.rb
@@ -7,10 +7,22 @@ require_relative './password.rb'
 class Imap
   def initialize
     @imap = Net::IMAP.new Configuration['host'], Configuration['port'], Configuration['use_ssl']
-    @imap.login Configuration['username'], Password.get
+    begin
+      @imap.login Configuration['username'], Password.get
+    rescue Net::IMAP::NoResponseError => error
+      $stderr.puts "IMAP: #{error.message}"
+      exit 1
+    end
   end
 
   def list(mailbox=nil)
     @imap.list(mailbox || '', '*')
+  end
+
+  def messages_for(mailbox)
+    @imap.examine mailbox
+    @imap.uid_search('ALL').each do |uid|
+      yield @imap.uid_fetch(uid,'RFC822').first.attr['RFC822']
+    end
   end
 end
