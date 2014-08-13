6d5d72817794508469a64763bc8e583e5d2260d5
Florian Störkle <florian@stoerkle.net>
2014-08-13 15:55:19 +0200

    Improve console output

--- lib/imap_json.rb
+++ lib/imap_json.rb
@@ -1,18 +1,27 @@
 
 require 'imap_json/config'
-require 'imap_json/email'
 require 'imap_json/files'
 require 'imap_json/imap'
+require 'imap_json/utils.rb'
 
 module ImapJson
+  @@imap = nil
+
+  def self.setup
+    puts
+    
+    Utils.console('Creating output directories') { Files.create_directories }
+
+    @@imap = Imap.new
+  end
+
   def self.run
-    imap = Imap.new
-    imap.list(Configuration['mailbox']).each do |mailbox|
-      imap.mails_for(mailbox.name) do |mail|
+    self.setup
+
+    @@imap.list(Configuration['mailbox']).each do |mailbox|
+      @@imap.mails_for(mailbox.name) do |mail|
         puts mail.subject
       end
-    end
-
-    # Files.create_directories
+    end 
   end
 end
--- lib/imap_json/utils.rb
+++ lib/imap_json/utils.rb
@@ -3,14 +3,21 @@ require 'highline/import'
 
 module Utils
   def self.read_password
-    ask('Enter IMAP password: ') { |q| q.echo = '*' }
+    puts
+    ask(' * Please enter IMAP password: ') { |q| q.echo = '*' }
   end
 
-  def error(message)
+  def self.console(start_message, stop_message='finished')
+    print ">> #{start_message}... "
+    yield
+    puts stop_message
+  end
+
+  def self.error(message)
     $stderr.puts message
   end
 
-  def fatal(message, code=1)
+  def self.fatal(message, code=1)
     error message
     exit code
   end
